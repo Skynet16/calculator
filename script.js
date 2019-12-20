a4243784838a961eb93e1e0add42263dea47d9db
Ivan <Vanes_kote@mail.ru>
2019-12-20 20:48:45 +0300

    Результат сложения

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function plus() {
-  var num1 , num2 , result;
-  num1 = document.getElementById('first_number').value;
-  num2 = document.getElementById('second_number').value;
-result = num1+num2;
-document.getElementById('result')
+function plus(){
+var addition = document.getElementsByName('addition');
+   var num1, num2, result;
+   num1 = document.getElementById('first_number').value;
+   num1 = parseInt(num1);
+   num2 = document.getElementById('second_number').value;
+   num2 = parseInt(num2);
+   result = num1+num2;
+   document.getElementById('result').innerHTML = result;
 }
