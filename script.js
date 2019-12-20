776d69bb4aae7bf7b257ac26d1eb026867830c97
Ivan <Vanes_kote@mail.ru>
2019-12-20 19:52:14 +0300

    Создаю функционал калькулятору

---
+++ script.js
@@ -0,0 +1,7 @@
+function plus() {
+  var num1 , num2 , result;
+  num1 = document.getElementById('first_number').value;
+  num2 = document.getElementById('second_number').value;
+result = num1+num2;
+document.getElementById('result')
+}
