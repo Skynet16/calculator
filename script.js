6916ff0a345dd6aad6cd7d17dcb68f9d73bb2fb2
Ivan <Vanes_kote@mail.ru>
2019-12-23 20:48:01 +0300

    Калькулятор

--- script.js
+++ script.js
@@ -1,50 +1,32 @@
-//создаю фунцию для сложения чисел
 function plus(){
-//пишу переменые для подсчёта
-var addition = document.getElementsByName('addition');
-   var num1, num2, result;
-   //первое число
    num1 = document.getElementById('first_number').value;
-   //избегаю конкатенации
    num1 = parseInt(num1);
-   //второе число
    num2 = document.getElementById('second_number').value;
    num2 = parseInt(num2);
-   //решение
-   result = num1+num2;
+   result = num1 + num2;
    document.getElementById('result').innerHTML = result;
 }
-//создаю фунцию для вычитания чисел
 function minus(){
-var subtraction = document.getElementsByName('subtraction');
-   var num3, num4, reshenie;
-   num3 = document.getElementById('first_number').value;
-   num3 = parseInt(num3);
-   num4 = document.getElementById('second_number').value;
-   num4 = parseInt(num4);
-   reshenie = num3-num4;
-   document.getElementById('result').innerHTML = reshenie;
+   num1 = document.getElementById('first_number').value;
+   num1 = parseInt(num1);
+   num2 = document.getElementById('second_number').value;
+   num2 = parseInt(num2);
+   result = num1 - num2;
+   document.getElementById('result').innerHTML = result;
 }
-//создаю фунцию для умножения чисел
 function multiply(){
-var multiplication = document.getElementsByName('multiplication');
-   var num5, num6, ready;
-   num5 = document.getElementById('first_number').value;
-   num5 = parseInt(num5);
-   num6 = document.getElementById('second_number').value;
-   num6 = parseInt(num6);
-   ready = num5*num6;
-   document.getElementById('result').innerHTML = ready;
+   num1 = document.getElementById('first_number').value;
+   num1 = parseInt(num1);
+   num2 = document.getElementById('second_number').value;
+   num2 = parseInt(num2);
+   result = num1 * num2;
+   document.getElementById('result').innerHTML = result;
 }
-//создаю фунцию для деления чисел
-function share(){
-var division = document.getElementsByName('division');
-   var num7, num8, go;
-   num7 = document.getElementById('first_number').value;
-   num7 = parseInt(num7);
-   num8 = document.getElementById('second_number').value;
-   num8 = parseInt(num8);
-   go = num7/num8;
-   document.getElementById('result').innerHTML = go;
+function divide(){
+   num1 = document.getElementById('first_number').value;
+   num1 = parseInt(num1);
+   num2 = document.getElementById('second_number').value;
+   num2 = parseInt(num2);
+   result = num1 / num2;
+   document.getElementById('result').innerHTML = result;
 }
-console.log(" Чудаков Иван Дмитриевич")
