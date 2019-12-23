837a4e7c9c5546f3b066a5964372390b501fcb43
Ivan <Vanes_kote@mail.ru>
2019-12-23 20:58:22 +0300

    Калькулятор

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+//Функция сложения
 function plus(){
+  //Переменные для функций
+  var num1 , num2 , result;
+  //Первое число
    num1 = document.getElementById('first_number').value;
+   //Обход конкатенации
    num1 = parseInt(num1);
+   //Второе число
    num2 = document.getElementById('second_number').value;
+   //Обход конкатенации
    num2 = parseInt(num2);
+   //Решение
    result = num1 + num2;
    document.getElementById('result').innerHTML = result;
 }
+//Функция вычитания
 function minus(){
    num1 = document.getElementById('first_number').value;
    num1 = parseInt(num1);
@@ -14,6 +23,7 @@ function minus(){
    result = num1 - num2;
    document.getElementById('result').innerHTML = result;
 }
+//Функция умножения
 function multiply(){
    num1 = document.getElementById('first_number').value;
    num1 = parseInt(num1);
@@ -22,6 +32,7 @@ function multiply(){
    result = num1 * num2;
    document.getElementById('result').innerHTML = result;
 }
+//Функция деления
 function divide(){
    num1 = document.getElementById('first_number').value;
    num1 = parseInt(num1);
@@ -30,3 +41,4 @@ function divide(){
    result = num1 / num2;
    document.getElementById('result').innerHTML = result;
 }
+console.log(" Калькулятор готов :) ");
